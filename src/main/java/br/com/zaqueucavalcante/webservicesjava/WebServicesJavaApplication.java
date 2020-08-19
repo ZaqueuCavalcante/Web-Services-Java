0aac43187dd36a7cb1f5e8b660a8790e2b61d263
Zaqueu C <zaqueudovale@gmail.com>
2020-08-18 21:36:41 -0300

    First commit.

---
+++ src/main/java/br/com/zaqueucavalcante/webservicesjava/WebServicesJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zaqueucavalcante.webservicesjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zaqueucavalcante/webservicesjava/WebServicesJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zaqueucavalcante.webservicesjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
